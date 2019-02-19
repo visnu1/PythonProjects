ab55c2d41cd3e89a55e8e15d1731e0d9d751b0b0
visnu1 <vishnu840ranjan@gmail.com>
2019-02-19 16:27:54 +0530

    oops

---
+++ Utility/utility_oops.py
@@ -0,0 +1,19 @@
+class Oops:
+
+    @staticmethod
+    def inventory_calculations(data):
+        rice_prices = []
+        wheat_prices = []
+        pulses_prices = []
+        for i, j, k in zip(data["rice"], data["wheat"], data["pulses"]):
+            rice_prices.append(str(i['name']) + " = " + str(i['price'] * i['weight']))
+            wheat_prices.append(str(j['name']) + " = " + str(j['price'] * j['weight']))
+            pulses_prices.append(str(k['name']) + " = " + str(k['price'] * k['weight']))
+        data = {}
+        data['Total groceries value'] = []
+        data['Total groceries value'].append({
+            "rice price":rice_prices,
+            "wheat price":wheat_prices,
+            "pulses_prices":pulses_prices
+        })
+        return data
--- oops/inventory_manager.py
+++ oops/inventory_manager.py
@@ -1,20 +1,18 @@
+from Utility.utility_oops import Oops
 import json
 
 
 class InventoryManager:
     try:
 
+        inventory = Oops()
         path = input(
             "Enter the file path: ")  # /home/bridgeit/pythonProjects/oops/json and text files/inventoryManager.json
         with open(path) as json_data:
             data = json.load(json_data)
-        rice = data.rice
-        wheat = data.wheat
-        pulses = data.pulses
-        total_price = 0
-        arr = []
-        for p in pulses:
-
+        data = inventory.inventory_calculations(data)
+        with open('data.txt', 'w') as outfile:
+            json.dump(data, outfile)
 
     except Exception as error:
         print(repr(error))
